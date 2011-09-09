7edcf0db8d87e07bd39d2eaa3f062bbd6b09dcfb
Kevin McQuown <kevin@deck5software.com>
2011-09-09 12:49:40 -0700

    Initial Commit

---
+++ cellarworxCrushPad/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  cellarworxCrushPad
+//
+//  Created by Kevin McQuown on 9/9/11.
+//  Copyright (c) 2011 deck5 Software, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
